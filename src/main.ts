944f2e01bc56fb3e99dd045dcde460858640eafc
robin-sevelin <robinsevelin@hotmail.com>
2022-12-07 09:11:30 +0100

    restored wireframe

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 import './style/style.scss';
+
+const myPosition = document.querySelector('.my-position');
+// const myString = 'hello world';
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.Latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error);
