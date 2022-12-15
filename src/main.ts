e2308dfa5b9fe69fa238e6465c3aad776c342068
robin-sevelin <robinsevelin@hotmail.com>
2022-12-15 15:03:57 +0100

    working ts layout

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,34 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import './style/style.scss';
 
-const temp: HTMLDivElement | null = document.querySelector('.weather-temp');
-const myLocation: HTMLDivElement | null = document.querySelector('.weather-location');
-const weatherDescription: HTMLDivElement | null = document.querySelector('.weather-description');
-const weatherIcon: HTMLDivElement | null = document.querySelector('.weather-icon');
-const errorContainer: HTMLDivElement | null = document.querySelector('.error-container');
+const weatherTemp = document.querySelector('.temperature-value p');
+const myLocation = document.querySelector('.location p');
+const weatherInfo = document.querySelector('.description p');
+const weatherIcon = document.querySelector('.icon');
+const errorContainer = document.querySelector('.error');
+const windContainer = document.querySelector('.wind p');
+const feelsLike = document.querySelector('.feels-like p');
+const localTimeContainer = document.querySelector('.local-time');
+const weatherBackground = document.querySelector('.weather-background');
 const key = 'bf8a6a9e6c78c59cdb9e6c5aa6b2eccc';
 const currentDate = new Date();
 
 function getWeather(position: GeolocationPosition) {
   const { latitude, longitude }: GeolocationCoordinates = position.coords;
   const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=metric&lang=sv&lat=${latitude}&lon=${longitude}&appid=${key}`;
-  console.log(latitude);
-  console.log(longitude);
-  console.log(apiUrl);
   fetch(apiUrl)
     .then((response) => response.json())
     .then((json) => {
-      console.log(json);
-      if (myLocation != null && temp != null && weatherDescription != null && weatherIcon != null) {
-        console.log('hej');
+      if (myLocation != null && weatherTemp != null && weatherInfo != null && weatherIcon != null && feelsLike != null && windContainer != null) {
         myLocation.innerHTML = `${json.city.name}, ${json.city.country}`;
-        temp.innerHTML = `${json.list[0].main.temp}, &#176;<span>C</span>`;
-        weatherDescription.innerHTML = `${json.list[0].weather[0].description}`;
+        weatherTemp.innerHTML = `${Math.round(json.list[0].main.temp)} &#176;<span>C</span>`;
+        feelsLike.innerHTML = `känns som ${Math.round(json.list[0].main.feels_like)}<span>&#176; C</span>`;
+        weatherInfo.innerHTML = json.list[0].weather[0].description;
         weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${json.list[0].weather[0].icon}.png" alt="" width="100" height="100" />`;
+        windContainer.innerHTML = `vind ${Math.round(json.list[0].wind.speed)} m/s`;
       }
     })
     .catch((error) => {
@@ -42,16 +45,24 @@ function getLocation() {
 }
 
 function checkDaytime() {
-  if (currentDate.getHours() < 7 || currentDate.getHours() > 20) {
-    if (currentDate.getMonth() > 9 || currentDate.getMonth() < 3) {
-      document.body.style.backgroundImage = 'url(background_imgs/winter-night-time.jpg)';
+  if (localTimeContainer != null) {
+    localTimeContainer.innerHTML = currentDate.toLocaleTimeString(
+      navigator.language,
+      { hour: '2-digit', minute: '2-digit' },
+    );
+  }
+  if (weatherBackground != null) {
+    if (currentDate.getHours() < 7 || currentDate.getHours() > 20) {
+      if (currentDate.getMonth() > 9 || currentDate.getMonth() < 3) {
+        weatherBackground.style.backgroundImage = 'url(background_imgs/winter-night-time.jpg)';
+      } else {
+        weatherBackground.style.backgroundImage = 'url(background_imgs/night-time.jpg)';
+      }
+    } else if (currentDate.getMonth() < 9 || currentDate.getMonth() > 3) {
+      weatherBackground.style.backgroundImage = 'url(background_imgs/winter-day-time.jpg)';
     } else {
-      document.body.style.backgroundImage = 'url(background_imgs/night-time.jpg)';
+      weatherBackground.style.backgroundImage = 'url(background_imgs/day-time.jpg)';
     }
-  } else if (currentDate.getMonth() < 9 || currentDate.getMonth() > 3) {
-    document.body.style.backgroundImage = 'url(background_imgs/winter-day-time.jpg)';
-  } else {
-    document.body.style.backgroundImage = 'url(background_imgs/day-time.jpg)';
   }
 }
 
