15d1535120ae813dfb22f63806e172e4c7a3577b
robin-sevelin <robinsevelin@hotmail.com>
2022-12-19 20:16:32 +0100

    comments

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-/* eslint-disable max-len */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable operator-linebreak */
 import './style/style.scss';
 
+// containers för sidan
 const weatherTemp = document.querySelector('.temperature-value p');
 const myLocation = document.querySelector('.location p');
 const weatherInfo = document.querySelector('.description p');
@@ -14,13 +16,17 @@ const windContainer = document.querySelector('.wind p');
 const feelsLike = document.querySelector('.feels-like p');
 const localTimeContainer = document.querySelector('.local-time p');
 const dateContainer = document.querySelector('.date p');
+// bakgrundsbilder
 const winterNightBackground = 'url(background-imgs/winter-night-time.webp)';
 const winterDayBackground = 'url(background-imgs/winter-day-time.webp)';
 const dayBackground = 'url(background-imgs/day-time.webp)';
 const nightBackground = 'url(background-imgs/night-time.webp)';
+// api nyckel
 const key = 'bf8a6a9e6c78c59cdb9e6c5aa6b2eccc';
+// datum variabel
 const currentDate = new Date();
 
+// funktion för att hämta och rendera väder data till sina containers
 async function getWeather(position: GeolocationPosition) {
   const { latitude, longitude }: GeolocationCoordinates = position.coords;
   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?id=524901&units=metric&lang=sv&lat=${latitude}&lon=${longitude}&appid=${key}`;
@@ -28,7 +34,14 @@ async function getWeather(position: GeolocationPosition) {
   await fetch(apiUrl)
     .then((response) => response.json())
     .then((json) => {
-      if (myLocation != null && weatherTemp != null && weatherInfo != null && weatherIcon != null && feelsLike != null && windContainer != null) {
+      if (
+        myLocation !== null &&
+        weatherTemp !== null &&
+        weatherInfo !== null &&
+        weatherIcon !== null &&
+        feelsLike !== null &&
+        windContainer !== null
+      ) {
         myLocation.innerHTML = `${json.city.name}, ${json.city.country}`;
         weatherTemp.innerHTML = `${Math.round(json.list[0].main.temp)}&#176;<span> C</span>`;
         feelsLike.innerHTML = `känns som ${Math.round(json.list[0].main.feels_like)}<span>&#176; C</span>`;
@@ -38,30 +51,30 @@ async function getWeather(position: GeolocationPosition) {
       }
     })
     .catch(() => {
-      if (errorContainer != null) {
+      if (errorContainer !== null) {
         errorContainer.innerHTML = 'kunde inte hämta data';
       }
     });
 }
 
+// funktion för att hämta position
 function getLocation() {
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(getWeather);
-  } else if (errorContainer != null) {
+  } else if (errorContainer !== null) {
     errorContainer.innerHTML = 'kunde inte hämta  data';
   }
 }
 
+// funktion för att hämta klockslag och datum
 function checkDaytime() {
   if (localTimeContainer != null && dateContainer != null) {
     dateContainer.innerHTML = currentDate.toLocaleDateString();
-    localTimeContainer.innerHTML = currentDate.toLocaleTimeString(
-      navigator.language,
-      {
-        hour: '2-digit', minute: '2-digit',
-      },
-    );
-  }
+    localTimeContainer.innerHTML = currentDate.toLocaleTimeString(navigator.language, {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  } // kontrollerar datum och tid
   if (currentDate.getHours() < 7 || currentDate.getHours() > 18) {
     if (currentDate.getMonth() > 9 || currentDate.getMonth() < 3) {
       document.body.style.backgroundImage = winterNightBackground;
@@ -74,5 +87,7 @@ function checkDaytime() {
     document.body.style.backgroundImage = dayBackground;
   }
 }
+// kallar datum funktion
 checkDaytime();
+// kallar position funktion
 getLocation();
