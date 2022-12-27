261cbb139bfef78df64b2cdc5f24662917c4eeaa
robin-sevelin <robinsevelin@hotmail.com>
2022-12-27 09:31:26 +0100

    error container

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@
 /* eslint-disable operator-linebreak */
 import './style/style.scss';
 
+const errorContainer = document.querySelector('.error-container') as HTMLElement;
 const weatherTemp = document.querySelector('.temperature-value p');
 const myLocation = document.querySelector('.location p');
 const weatherInfo = document.querySelector('.description p');
@@ -13,14 +14,10 @@ const windContainer = document.querySelector('.wind p');
 const feelsLike = document.querySelector('.feels-like p');
 const localTimeContainer = document.querySelector('.local-time p');
 const dateContainer = document.querySelector('.date p');
+const visibilityContainer = document.querySelector('.visibility');
 const findPositionButton = document.querySelector('#get-position');
-const winterNightBackground = 'url(background-imgs/winter-night-time.webp)';
-const winterDayBackground = 'url(background-imgs/winter-day-time.webp)';
-const dayBackground = 'url(background-imgs/day-time.webp)';
-const nightBackground = 'url(background-imgs/night-time.webp)';
-const key = 'bf8a6a9e6c78c59cdb9e6c5aa6b2eccc';
 const currentDate = new Date();
-
+const key = 'bf8a6a9e6c78c59cdb9e6c5aa6b2eccc';
 const weather = {
   city: '',
   country: '',
@@ -29,6 +26,14 @@ const weather = {
   description: '',
   icon: '',
   wind: 0,
+  visibility: 0,
+};
+
+const backgrounds = {
+  winterNightBackground: 'url(background-imgs/winter-night-time.webp)',
+  winterDayBackground: 'url(background-imgs/winter-day-time.webp)',
+  dayBackground: 'url(background-imgs/day-time.webp)',
+  nightBackground: 'url(background-imgs/night-time.webp)',
 };
 
 function renderWeather() {
@@ -38,20 +43,23 @@ function renderWeather() {
     weatherInfo !== null &&
     weatherIcon !== null &&
     feelsLike !== null &&
-    windContainer !== null
+    windContainer !== null &&
+    visibilityContainer !== null
   ) {
     myLocation.innerHTML = `${weather.city}, ${weather.country}`;
     weatherTemp.innerHTML = `${weather.temp}&#176;<span> C</span>`;
     feelsLike.innerHTML = `känns som ${weather.feels_like}<span>&#176; C</span>`;
     weatherInfo.innerHTML = weather.description;
-    weatherIcon.innerHTML = `<img src="/weather-icons/${weather.icon}.png" alt="${weather.description}" width="100" height="100" />`;
+    weatherIcon.innerHTML = `<img src="/weather-icons/${weather.icon}.png" loading="lay" alt="${weather.description}" width="100" height="100" />`;
     windContainer.innerHTML = `vind ${weather.wind} m/s`;
+    visibilityContainer.innerHTML = `sikt ${weather.visibility} km`;
   }
 }
 
 function showError() {
-  if (myLocation !== null) {
-    myLocation.innerHTML = 'lyckades inte hämta data';
+  if (errorContainer !== null) {
+    errorContainer.style.display = 'block';
+    errorContainer.innerHTML = '"lyckades inte hämta data"';
   }
 }
 
@@ -69,7 +77,7 @@ async function getWeather(position: GeolocationPosition) {
       weather.feels_like = Math.round(json.list[0].main.feels_like);
       weather.icon = json.list[0].weather[0].icon;
       weather.wind = Math.round(json.list[0].wind.speed);
-      console.log(weather);
+      weather.visibility = Math.round(json.list[0].visibility / 1000);
       renderWeather();
     })
     .catch(showError);
@@ -88,14 +96,14 @@ if (findPositionButton !== null) {
 function renderBackground() {
   if (currentDate.getHours() < 7 || currentDate.getHours() > 17) {
     if (currentDate.getMonth() > 9 || currentDate.getMonth() < 3) {
-      document.body.style.backgroundImage = winterNightBackground;
+      document.body.style.backgroundImage = backgrounds.winterNightBackground;
     } else {
-      document.body.style.backgroundImage = nightBackground;
+      document.body.style.backgroundImage = backgrounds.nightBackground;
     }
   } else if (currentDate.getMonth() < 9 || currentDate.getMonth() > 3) {
-    document.body.style.backgroundImage = winterDayBackground;
+    document.body.style.backgroundImage = backgrounds.winterDayBackground;
   } else {
-    document.body.style.backgroundImage = dayBackground;
+    document.body.style.backgroundImage = backgrounds.dayBackground;
   }
 }
 
