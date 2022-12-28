58e9e319e1ebdae668b4ca409b4f9c1e56c7b67e
robin-sevelin <robinsevelin@hotmail.com>
2022-12-28 19:43:46 +0100

    3 hour forecast

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@
 /* eslint-disable operator-linebreak */
 import './style/style.scss';
 
+const forecastThreeHour = document.querySelector('.three-hour');
+const forecastSixHour = document.querySelector('.six-hour');
+const forecastNineHour = document.querySelector('.nine-hour');
 const errorContainer = document.querySelector('.error-container') as HTMLElement;
 const weatherTemp = document.querySelector('.temperature-value p');
 const myLocation = document.querySelector('.location p');
@@ -18,6 +21,7 @@ const visibilityContainer = document.querySelector('.visibility');
 const findPositionButton = document.querySelector('#get-position');
 const currentDate = new Date();
 const key = 'bf8a6a9e6c78c59cdb9e6c5aa6b2eccc';
+
 const weather = {
   city: '',
   country: '',
@@ -27,6 +31,24 @@ const weather = {
   icon: '',
   wind: 0,
   visibility: 0,
+};
+
+const forecast = {
+  threeHour: {
+    time: '',
+    temp: 0,
+    icon: '',
+  },
+  sixHour: {
+    time: '',
+    temp: 0,
+    icon: '',
+  },
+  nineHour: {
+    time: '',
+    temp: 0,
+    icon: '',
+  },
 };
 
 const backgrounds = {
@@ -44,7 +66,10 @@ function renderWeather() {
     weatherIcon !== null &&
     feelsLike !== null &&
     windContainer !== null &&
-    visibilityContainer !== null
+    visibilityContainer !== null &&
+    forecastThreeHour !== null &&
+    forecastSixHour !== null &&
+    forecastNineHour !== null
   ) {
     myLocation.innerHTML = `${weather.city}, ${weather.country}`;
     weatherTemp.innerHTML = `${weather.temp}&#176;<span> C</span>`;
@@ -53,6 +78,15 @@ function renderWeather() {
     weatherIcon.innerHTML = `<img src="./weather-icons/${weather.icon}.png" loading="lay" alt="${weather.description}" width="100" height="100" />`;
     windContainer.innerHTML = `vind ${weather.wind} m/s`;
     visibilityContainer.innerHTML = `sikt ${weather.visibility} km`;
+    forecastThreeHour.innerHTML = `<p>${forecast.threeHour.time}</p>
+    <img src="./weather-icons/${forecast.threeHour.icon}.png" loading="lay" alt="${weather.description}" width="50" height="50" />
+    <p>${forecast.threeHour.temp}&#176;<span> C</span></p>`;
+    forecastSixHour.innerHTML = `<p>${forecast.sixHour.time}</p>
+    <img src="./weather-icons/${forecast.sixHour.icon}.png" loading="lay" alt="${weather.description}" width="50" height="50" />
+    <p>${forecast.sixHour.temp}&#176;<span> C</span></p>`;
+    forecastNineHour.innerHTML = `<p>${forecast.nineHour.time}</p>
+    <img src="./weather-icons/${forecast.nineHour.icon}.png" loading="lay" alt="${weather.description}" width="50" height="50" />
+    <p>${forecast.nineHour.temp}&#176;<span> C</span></p>`;
   }
 }
 
@@ -78,6 +112,24 @@ async function getWeather(position: GeolocationPosition) {
       weather.icon = json.list[0].weather[0].icon;
       weather.wind = Math.round(json.list[0].wind.speed);
       weather.visibility = Math.round(json.list[0].visibility / 1000);
+      forecast.threeHour.time = new Date(1672272000 * 1000).toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      forecast.threeHour.temp = Math.round(json.list[1].main.temp);
+      forecast.threeHour.icon = json.list[1].weather[0].icon;
+      forecast.sixHour.time = new Date(1672282800 * 1000).toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      forecast.sixHour.temp = Math.round(json.list[2].main.temp);
+      forecast.sixHour.icon = json.list[2].weather[0].icon;
+      forecast.nineHour.time = new Date(1672293600 * 1000).toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      forecast.nineHour.temp = Math.round(json.list[3].main.temp);
+      forecast.nineHour.icon = json.list[3].weather[0].icon;
       renderWeather();
     })
     .catch(showError);
